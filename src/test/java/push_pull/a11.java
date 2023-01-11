ccc9cd4c00c08caa665d16209a9b922134f49a1f
ganeshpatil1012 <122169384+ganeshpatil1012@users.noreply.github.com>
2023-01-11 16:44:54 +0530

    Update a11.java

--- src/test/java/push_pull/a11.java
+++ src/test/java/push_pull/a11.java
@@ -10,6 +10,14 @@ System.out.println("push pull"
 		+ "    git git push origin master"
 		+ ""
 		+ "B)  git pull origin master");
+		
+		System.out.println("push pull"
+		+ "A)  git ststus -------press enter"
+		+ "    git add + untracked file path"
+		+ "    git commit -m ;commit text"
+		+ "    git git push origin master"
+		+ ""
+		+ "B)  git pull origin master");
 	}
 
 }
